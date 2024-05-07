c8648226ee6586d99fb9c81d02731e22b10a9a0e
ChrisC-55 <unlchriscox@gmail.com>
2024-05-07 10:32:38 -0500

    Add project files.

---
+++ Models/Curse.cs
@@ -0,0 +1,12 @@
+﻿namespace HaveANiceDeath.Models
+{
+    public class Curse
+    {
+        public int CurseID { get; set; }
+        public string CurseColor { get; set; }
+        public string CurseRarity { get; set; }
+        public string CurseType { get; set; }
+        public string CurseDescription { get; set; }
+        public int CurseRank { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HaveANiceDeath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
